f5fa50414d0f0d182653571084108bcf43559f31
ichsanashiddiqi <iashiddiqi13@gmail.com>
2019-06-11 17:19:38 +0700

    penambahan anak dan lemari

---
+++ Assets/Standard Assets/DOOR3.js
@@ -0,0 +1,45 @@
+﻿var doorClip : AnimationClip;
+var Key : GameObject;
+private var Door2 = false;
+public var guiSkin : GUISkin;
+
+
+function Start () 
+{
+
+}
+
+function Update () 
+{
+	if (Input.GetKeyDown(KeyCode.E) && Door2 == true && Key.active == false)
+	{
+	GameObject.Find("Door2").animation.Play("lemariBuka");
+	}
+}
+
+function OnTriggerEnter (theCollider : Collider)
+{
+	if (theCollider.tag == "Player")
+	{
+		Door2 = true;
+	}
+}
+
+function OnTriggerExit (theCollider : Collider)
+{
+	if (theCollider.tag == "Player")
+	{
+		Door2 = false;
+	}
+}
+
+function OnGUI ()
+{
+	if(Door2 == true)
+	{
+		GUI.skin = guiSkin;
+		GUI.Label (Rect (Screen.width/2-50, Screen.height/2-55, 120, 50), "Press E to Open The Door");
+	
+	}
+
+}
---
+++ Assets/Standard Assets/tembokBuka.js
@@ -0,0 +1,45 @@
+﻿var doorClip : AnimationClip;
+var Key : GameObject;
+private var Door3 = false;
+public var guiSkin : GUISkin;
+
+
+function Start () 
+{
+
+}
+
+function Update () 
+{
+	if (Input.GetKeyDown(KeyCode.E) && Door3 == true && Key.active == false)
+	{
+	GameObject.Find("Door3").animation.Play("lemariAnim");
+	}
+}
+
+function OnTriggerEnter (theCollider : Collider)
+{
+	if (theCollider.tag == "Player")
+	{
+		Door3 = true;
+	}
+}
+
+function OnTriggerExit (theCollider : Collider)
+{
+	if (theCollider.tag == "Player")
+	{
+		Door3 = false;
+	}
+}
+
+function OnGUI ()
+{
+	if(Door3 == true)
+	{
+		GUI.skin = guiSkin;
+		GUI.Label (Rect (Screen.width/2-50, Screen.height/2-55, 120, 50), "Press E to Open The Door");
+	
+	}
+
+}
