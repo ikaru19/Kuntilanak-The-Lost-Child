f61bbc0dffba5b709e2cb4491b256b4f6ecf49d9
ichsanashiddiqi <iashiddiqi13@gmail.com>
2019-06-11 17:23:10 +0700

    penambahan anak dan lemari(door)

--- Assets/Scripts/EnemyMove.js
+++ Assets/Scripts/EnemyMove.js
@@ -1,27 +1,30 @@
 ﻿var Player : Transform;
-var MoveSpeed = 4;
-var MinDist =5;
+var MoveSpeed : float = 4;
+var MinDist = 5;
 var MaxDist = 10;
+var buttonInRange;
 
 
 function Start()
 {
-
+	buttonInRange = false;
 }
 
 function Update ()
 {
 	//Moves towrd player
-	transform.LookAt (Player);
-	if(Vector3.Distance(transform.position,Player.position ) >= MinDist) {
 	
-		transform.position += transform.forward * MoveSpeed*Time.deltaTime;
+	
+		transform.LookAt (Player);
+		if(Vector3.Distance(transform.position,Player.position ) >= MinDist) {
 		
-		
-		if(Vector3.Distance(transform.position,Player.position) <= MaxDist)
-		{
-				Destroy(this.gameObject);
-		
+			transform.position += transform.forward * MoveSpeed*Time.deltaTime;
+			
+			
+			if(Vector3.Distance(transform.position,Player.position) <= MaxDist)
+			{
+					MoveSpeed += 0.009;
+			
+			}
 		}
-	}
 }
